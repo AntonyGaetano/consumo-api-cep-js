a222dce88414fc0db8cf047f5b90922459aa3640
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-10-17 14:55:47 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const preencherCampos = (endereco) =>{
+    document.getElementById("endereco").value = endereco.logradouro;
+    document.getElementById("bairro").value = endereco.bairro;
+    document.getElementById("cidade").value = endereco.localidade;
+    document.getElementById("estado").value = endereco.uf;
+}
+
+const buscaCep = async() =>{
+    const cep = document.getElementById("cep").value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const dados = await fetch(url)
+    const endereco = await dados.json();
+    preencherCampos(endereco);
+}
+
+
+document.getElementById("cep").addEventListener("focusout",buscaCep);
