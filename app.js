00ac422086982091008ec802c96a3137ec0498ac
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-10-17 15:27:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,34 @@ const preencherCampos = (endereco) =>{
     document.getElementById("estado").value = endereco.uf;
 }
 
+const limparCampos = () =>{
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const ValidandoCep = (cep) => cep.length == 8 && eNumero(cep); 
+ 
 const buscaCep = async() =>{
+    limparCampos();
     const cep = document.getElementById("cep").value;
     const url = `https://viacep.com.br/ws/${cep}/json/`;
-    const dados = await fetch(url)
-    const endereco = await dados.json();
-    preencherCampos(endereco);
+    if(ValidandoCep(cep)){
+      const dados = await fetch(url)
+      const endereco = await dados.json();
+      if(endereco.hasOwnProperty("erro")){
+        document.getElementById('endereco').value = 'CEP não encontrado!';
+      }
+      else{
+      preencherCampos(endereco);
+      }
+    }
+    else{
+        document.getElementById('endereco').value = 'CEP incorreto!';
+    }
 }
 
 
